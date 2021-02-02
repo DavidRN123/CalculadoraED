81680e56d595cecc0244530321da0572fc45befc
Doctor32 <luisrobbe32@gmail.com>
2021-02-02 19:34:55 +0100

    Funciones aritméticas

--- CalculadoraED/src/main/java/com/mycompany/calculadoraed/Calculadora.java
+++ CalculadoraED/src/main/java/com/mycompany/calculadoraed/Calculadora.java
@@ -2,5 +2,15 @@
 package com.mycompany.calculadoraed;
 
 public class Calculadora {
-    
+    public static double op(char operator, double num1, double num2) {
+        switch(operator) {
+            case '+': return num1 + num2;
+            case '-': return num1 - num2;
+            case '*': return num1 * num2;
+            case '/': return num1 / num2;
+            case '%': return num1 % num2;
+            case '^': return Math.pow(num1, num2);
+            default: return 0;
+        }
+    }
 }
