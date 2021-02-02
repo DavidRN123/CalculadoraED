3af47c851577b47cecf9ba729806f4914ce480b4
Mena-Dani <rodriguezmenadaniel@gmail.com>
2021-02-02 19:31:31 +0100

    Operaciones aritmeticas

--- CalculadoraED/src/main/java/com/mycompany/calculadoraed/Main.java
+++ CalculadoraED/src/main/java/com/mycompany/calculadoraed/Main.java
@@ -3,6 +3,22 @@ package com.mycompany.calculadoraed;
 
 public class Main {
     public static void main(String[] args) {
+         Scanner input = new Scanner (System.in);
+        boolean quit=false;
+        double numero=0;
+        String dato = "";
+        double numero2=0;
         
+        while (!quit) {
+            System.out.println("["+numero+"]");
+            dato =  input.nextLine();
+            if (esNumero(dato)){
+                if(negativo(dato)) {numero = -(convertirANumero(dato));}
+                else {numero = convertirANumero(dato);}
+            } else {
+                if(dato.equals("+") || dato.equals("-") || dato.equals("*") || dato.equals("/") || dato.equals("%") || dato.equals("^")) {
+                    numero2 = input.nextDouble();
+                    numero = Calculadora.op(dato.charAt(0), numero, numero2);
+                }
     }
 }
